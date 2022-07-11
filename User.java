28cafae29dfe98260757e9838a164eab29a57a1e
Sanjithj <sanjithj591997@gmail.com>
2022-07-11 20:13:43 +0530

    Added: UC2_Last Name

--- User.java
+++ User.java
@@ -5,9 +5,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class User {
-	public static void nameValidation(String name) {
-		String nameregex="^[A-Z]{1}[a-z]{2}$";
-		Pattern pattern=Pattern.compile(nameregex);
+	public static void firstNameValidation(String name) {
+		String firstNameRegex="^[A-Z]{1}[a-z]{2}$";
+		Pattern pattern=Pattern.compile(firstNameRegex);
 		Matcher matcher=pattern.matcher(name);
 		if(matcher.find()) {
 			System.out.println("its valid");
@@ -17,13 +17,28 @@ public class User {
 		}
 		
 	}
+public static void lastNameValidation(String name) {
+	String lastNameRegex="^[A-Z]{1}[a-z]{2}$";
+	Pattern pattern=Pattern.compile(lastNameRegex);
+	Matcher matcher=pattern.matcher(name);
+	if(matcher.find()) {
+		System.out.println("its valid");
+	}
+	else {
+		System.out.println("its not valid");
+	}
+
+		
+	}
 public static void main(String[] args) {
 	System.out.println("Welcome to user registration");
 	Scanner sc=new Scanner(System.in);
 	System.out.println("Enter the first name");
 	String firstName = sc.next();
-	User.nameValidation(firstName);
-
-
+	User.firstNameValidation(firstName);
+	System.out.println("Enter the last name");
+	String lastName = sc.next();
+    User.lastNameValidation(lastName);
+    sc.close();
 }
 }
